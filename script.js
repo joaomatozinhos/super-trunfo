ddcaed67df99b52b8462976c506d464024539092
João Pedro Matozinhos <jpmatozinhos@gmail.com>
2021-09-30 22:33:06 -0300

    add script.js

---
+++ script.js
@@ -0,0 +1,142 @@
+var carta1 = {
+  nome: 'Lionel Messi',
+  geral: 93,
+  atributos: {
+    ritmo: 85,
+    finalizacao: 92,
+    passe: 91,
+    drible: 95,
+    defesa: 38,
+    físico: 65
+  }
+}
+
+var carta2 = {
+  nome: 'Cristiano Ronaldo',
+  geral: 92,
+  atributos: {
+    ritmo: 89,
+    finalizacao: 93,
+    passe: 81,
+    drible: 89,
+    defesa: 35,
+    físico: 77
+  }
+}
+
+var carta3 = {
+  nome: 'Neymar Jr.',
+  geral: 91,
+  atributos: {
+    ritmo: 91,
+    finalizacao: 85,
+    passe: 86,
+    drible: 94,
+    defesa: 36,
+    físico: 59
+  }
+}
+
+var carta4 = {
+  nome: 'Kylian Mbappé',
+  geral: 90,
+  atributos: {
+    ritmo: 96,
+    finalizacao: 86,
+    passe: 78,
+    drible: 91,
+    defesa: 39,
+    físico: 76
+  }
+}
+
+var carta5 = {
+  nome: 'Robert Lewandowski',
+  geral: 91,
+  atributos: {
+    ritmo: 78,
+    finalizacao: 91,
+    passe: 78,
+    drible: 86,
+    defesa: 43,
+    físico: 82
+  }
+}
+
+var carta6 = {
+  nome: 'Kevin de Bruyne',
+  geral: 91,
+  atributos: {
+    ritmo: 76,
+    finalizacao: 86,
+    passe: 93,
+    drible: 88,
+    defesa: 64,
+    físico: 78
+  }
+}
+
+var carta7 = {
+  nome: 'Mohamed Salah',
+  geral: 90,
+  atributos: {
+    ritmo: 93,
+    finalizacao: 86,
+    passe: 81,
+    drible: 90,
+    defesa: 45,
+    físico: 75
+  }
+}
+
+var carta8 = {
+  nome: 'Sadio Mané',
+  geral: 90,
+  atributos: {
+    ritmo: 94,
+    finalizacao: 85,
+    passe: 80,
+    drible: 90,
+    defesa: 44,
+    físico: 76
+  }
+}
+
+var carta9 = {
+  nome: 'Harry Kane',
+  geral: 88,
+  atributos: {
+    ritmo: 68,
+    finalizacao: 91,
+    passe: 80,
+    drible: 81,
+    defesa: 47,
+    físico: 83
+  }
+}
+
+var carta10 = {
+  nome: 'Toni Kroos',
+  geral: 88,
+  atributos: {
+    ritmo: 54,
+    finalizacao: 81,
+    passe: 91,
+    drible: 81,
+    defesa: 71,
+    físico: 69
+  }
+}
+
+var cartas = [
+  carta1,
+  carta2,
+  carta3,
+  carta4,
+  carta5,
+  carta6,
+  carta7,
+  carta8,
+  carta9,
+  carta10
+]
